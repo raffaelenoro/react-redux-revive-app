00eee8df83d823fdc712d0be905f9c2b9d370b91
raffaelenoro@hotmail.com <raffaelenoro@hotmail.com>
2020-09-07 21:29:40 +0000

    Stub of tables

--- src/components/Home/Tables.js
+++ src/components/Home/Tables.js
@@ -10,8 +10,66 @@ const Tables = props => {
     const tables = props.tables;
 
     if (tables) {
-        return(
-            <div>Tables: {props.tablesCount}</div>
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-md-3">
+                        {
+                            tables.filter(
+                                (table, index) => (index % 4) == 0
+                            ).map(table => {
+                                return (
+                                    <div key={"table_area_" + table.index}>
+                                        {table.c1_name}
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+
+                    <div className="col-md-3">
+                        {
+                            tables.filter(
+                                (table, index) => (index % 4) == 1
+                            ).map(table => {
+                                return (
+                                    <div key={"table_area_" + table.index}>
+                                        {table.c1_name}
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+
+                    <div className="col-md-3">
+                        {
+                            tables.filter(
+                                (table, index) => (index % 4) == 2
+                            ).map(table => {
+                                return (
+                                    <div key={"table_area_" + table.index}>
+                                        {table.c1_name}
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+
+                    <div className="col-md-3">
+                        {
+                            tables.filter(
+                                (table, index) => (index % 4) == 3
+                            ).map(table => {
+                                return (
+                                    <div key={"table_area_" + table.index}>
+                                        {table.c1_name}
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
         );
     } else {
         return (
