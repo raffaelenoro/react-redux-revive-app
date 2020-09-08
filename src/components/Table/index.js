24d0fea5a256b802d9a51eef8ad6bf1d827efafe
raffaelenoro@hotmail.com <raffaelenoro@hotmail.com>
2020-09-08 18:26:38 +0000

    Added tabulation of data

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,14 +1,62 @@
 import React from 'react';
 import { useTable } from 'react-table';
 
+const ShowTable = (props) => {
+    const table = props.table;
+
+    const columns = [{
+        Header: table.c1_name,
+        accessor: 'c1'
+    }, {
+        Header: table.c2_name,
+        accessor: 'c2'
+    }];
+
+    const data = table.data.map(
+        datum => ({ c1: datum.c1_data, c2: datum.c2_data})
+    );
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({columns, data});
+
+    return (
+        <table {...getTableProps()}>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                    ))}
+                </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                prepareRow(row)
+                return (
+                    <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => {
+                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    })}
+                    </tr>
+                )
+                })}
+            </tbody>
+        </table>
+    );
+}
+
 class Table extends React.Component {
     render() {
         const table = this.props.table;
 
         return (
-            <div>
-                {table.c1_name}
-            </div>
+            <ShowTable table={table}/>
         );
     }
 };
