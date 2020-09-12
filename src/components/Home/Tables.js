3ba05c56ed0921a0a7862ff905f32282967f0c99
raffaelenoro@hotmail.com <raffaelenoro@hotmail.com>
2020-09-12 02:29:54 +0000

    Added dimension selection

--- src/components/Home/Tables.js
+++ src/components/Home/Tables.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Table } from '../Table';
-
+import Select from 'react-select';
 
 const mapStateToProps = state => ({
   ...state.tableList
 });
 
 const Tables = props => {
+    const dimensions = [{label: "Billed", value: "billed"}];
     const tables = props.tables;
 
     if (tables) {
         return (
             <div>
+                <div className="row">
+                    <div className="col-md-6"></div>
+                    <div className="col-md-3">
+                        <span className="align-middle">Dimension By: </span>
+                    </div>
+                    <div className="col-md-3">
+                        <Select options={dimensions} /> 
+                    </div>
+                </div>
+
                 <div className="row">
                     <div className="col-md-3">
                         {
